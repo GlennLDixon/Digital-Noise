8dacde819b43c8e273847987e486410f6b70be42
Glenn Dixon Jr <glenndixon19@gmail.com>
2022-03-12 10:01:43 -0600

    using a function to fetch data from an external API to search songs by artistName and trackName

--- src/modules/PlayListManager.js
+++ src/modules/PlayListManager.js
@@ -1,11 +1,43 @@
-export const getPlayLists = () => {
-    fetch("https://spotify23.p.rapidapi.com/playlist/?id=37i9dQZF1DX4Wsb4d7NKfP", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "spotify23.p.rapidapi.com",
-		"x-rapidapi-key": "c18d2254bamsh3f06d44bec19ee0p13fe26jsnb057da6bc53e"
-	}
-})
-.then(result => result.json())
-.then(data => {return data})
-}
+const apiKey = process.env.REACT_APP_GLENN
+const apiHost = "theaudiodb.p.rapidapi.com"
+const apiUrl = "https://theaudiodb.p.rapidapi.com"
+
+export const getTracksByArtistAndSong = async (artistName, trackName) => {
+	try {
+		const response = await fetch(
+			`${apiUrl}/searchtrack.php?s=${artistName}&t=${trackName}`, {
+			"method": "GET",
+			"headers": {
+				"x-rapidapi-host": apiHost,
+				"x-rapidapi-key": apiKey
+			}
+		});
+		
+		let data = await response.json();
+		data = data.track;
+
+		return data;
+		} catch (error) {
+			console.log(error);
+		}
+};
+
+
+// export const getTracks = async (trackId) => {
+// 	try {
+// 	const response = await fetch(
+// 		`${apiUrl}track.php?h=${trackId}`,
+// 		{
+// 		method: "GET",
+// 		headers: {
+// 			"x-rapidapi-host": apiHost,
+// 			"x-rapidapi-key": apiKey
+// 		},
+// 	});
+
+// 	const data = await response.json();
+// 	return data;
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// };
