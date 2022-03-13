38ba530902a121f429dbb63d01bf07b6dd51548b
Glenn Dixon Jr <glenndixon19@gmail.com>
2022-03-13 12:20:53 -0500

    created a function that has props that will have details for the track that is retrieved from the api

---
+++ src/components/explore/ExploreSongCard.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export const ExploreSongCard = ({trackId, albumArt,trackTitle, albumTitle, trackDuration}) => { 
+return (
+    <div key={trackId}>
+        <div className="SongList-checkmark"></div>
+        <img className="SongList-albumArt"src={albumArt} />
+        <div className="SongList-trackTitle">{trackTitle}</div>
+        <div className="SongList-albumTitle">{albumTitle}</div>
+        <div className="SongList-trackDuration">{trackDuration}</div> {/** Convert to mins and secs*/}
+    </div>
+    )
+}
