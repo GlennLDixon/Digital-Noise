bbd59334771e6b63913564f9ed42cad954d6ad0c
Glenn Dixon Jr <glenndixon19@gmail.com>
2022-03-22 15:29:13 -0500

    changed the className to playlistcontainer and added a return in from of the playlistsongcard component to display the data one the screen also added tracks to the playlist useState

--- src/components/playlist/PlayListDetails.js
+++ src/components/playlist/PlayListDetails.js
@@ -4,6 +4,48 @@ import { getTrackById, getPlayLists, getPlayListById } from "../../modules/PlayL
 import {ExploreSongCard} from '../explore/ExploreSongCard'
 import { PlayListSongCard } from "./PlayListSongCard.js";
 
+
+export const PlayListDetails = () => {
+    const [playList, setPlayList] = useState({tracks:[]})
+    const [isLoading, setIsLoading] = useState(true)
+
+    const { id } = useParams();
+    
+    
+    useEffect(() => {
+        
+        getPlayListById(id).then((list) => {
+            console.log(list);
+            setPlayList(list);
+        });
+        setIsLoading(false);
+        // getTrackById().then(apiTrackId => {
+            //     setReturnedTrackId(apiTrackId)
+        // })
+    }, []);
+
+    
+    console.log(playList.tracks)
+    return (
+        <>
+        <div className="playListContainer">
+            {playList.tracks.map((track) => {
+            return <PlayListSongCard
+                    key={track.id}    
+                    tracks={track}
+                />
+            })}
+            
+        </div>
+        </>
+    )
+}
+// useEffect(() => {
+//     getPlayLists().then(list =>{
+//         setPlayLists(list)
+//     })
+// }, [])
+
 // const dummyData = {
 //     "title": "NewPlayList",
 //     "CreatorName": "Glenn Dixon",
@@ -13,75 +55,17 @@ import { PlayListSongCard } from "./PlayListSongCard.js";
 //     "id": 2
 // };
 
-export const PlayListDetails = () => {
-    const [returnedTrackId, setReturnedTrackId] = useState('')
-    const [playList, setPlayList] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
 
-    const { id } = useParams();
 
-    // const test = async (id) => {
-    //     const list = await getPlayListById(id);
-    //     console.log('RETURNED FROM DB: ', list);
-    //     setPlayList(list);
-    //     console.log('Playlist: ', playList);
-    //     setIsLoading(false);
-    // }
+// const test = async (id) => {
+//     const list = await getPlayListById(id);
+//     console.log('RETURNED FROM DB: ', list);
+//     setPlayList(list);
+//     console.log('Playlist: ', playList);
+//     setIsLoading(false);
+// }
 
-    useEffect(() => {
-        
-        getPlayListById(id).then((list) => {
-            console.log(list);
-            setPlayList(list);
-        });
-        setIsLoading(false);
-        // getTrackById().then(apiTrackId => {
-        //     setReturnedTrackId(apiTrackId)
-        // })
-    }, []);
 
-    // useEffect(() => {
-    //     getPlayLists().then(list =>{
-    //         setPlayLists(list)
-    //     })
-    // }, [])
-
-    // useEffect(() => {
-    //     //getAnimalById(id) from AnimalManager and hang on to the data; put it into state
-    //     console.log("useEffect", animalId)
-    //     getAnimalById(animalId)
-    //     .then(animal => {
-    //         setAnimal(animal);
-    //         setIsLoading(false)
-    //     });
-    // }, [playlistId]);
-    console.log(playList.tracks)
-    return (
-        <>
-        <div className="container">
-            {playList.tracks.map((track) => {
-                    <PlayListSongCard
-                        key={track.id}    
-                        tracks={track}
-                    />
-                })}
-            
-            {/* {
-                isLoading ? (<p>Loading...</p>) : (<>{playList.tracks.map((track) => {
-                    <ExploreSongCard
-                        key={track.id}    
-                        trackId={track.idTrack}
-                        albumArt={track.strTrackThumb}
-                        trackTitle={track.strTrack}
-                        albumTitle={track.strAlbum}
-                        trackDuration={track.intDuration}
-                    />
-                })}</>)
-            } */}
-        </div>
-        </>
-    )
-}
 
 // playList.tracks.map((track) => {
 //                     <ExploreSongCard
@@ -97,7 +81,7 @@ export const PlayListDetails = () => {
 
 
 // isLoading ? (<p>Loading...</p>) : (<>{playList.tracks.map((track) => {
-                //     <ExploreSongCard
+    //     <ExploreSongCard
                 //         key={track.id}    
                 //         trackId={track.idTrack}
                 //         albumArt={track.strTrackThumb}
